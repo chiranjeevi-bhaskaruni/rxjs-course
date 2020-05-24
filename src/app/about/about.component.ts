37b1d262b563f52155270a1a610f00a5ac51203e
Chiranjeevi Bhaskaruni <bhaskarunichiranjeevi@gmail.com>
2020-05-23 22:53:11 -0500

    Update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject, AsyncSubject, ReplaySubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -13,6 +13,39 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+      /* const sub = new Subject();
+      sub.subscribe(val => console.log("First : ", val));
+      
+      sub.next(1);
+      sub.subscribe(val => console.log("Second : ", val));
+      sub.next(2); */
+
+     /*  const sub = new BehaviorSubject<any>('test');
+      sub.subscribe(val => console.log("First : ", val));
+      
+      sub.next(1);
+      sub.subscribe(val => console.log("Second : ", val));
+      sub.next(2); */
+
+      /* const sub = new AsyncSubject();
+
+      sub.subscribe(val => console.log("First : ", val));
+      
+      sub.next(1);
+      sub.next(3);
+      sub.complete();
+      sub.subscribe(val => console.log("Second : ", val));
+      sub.next(2); */
+
+      /* const sub = new ReplaySubject();
+
+      sub.subscribe(val => console.log("First : ", val));
+      
+      sub.next(1);
+      sub.next(3);
+      sub.complete();
+      sub.subscribe(val => console.log("Second : ", val));
+      sub.next(2); */
 
     }
 
